b05920e9b05130efea887ad7a92fa9d1171b897d
Razakai <56027711+Razakai@users.noreply.github.com>
2021-03-12 19:10:42 +0000

    got cors working

--- run.py
+++ run.py
@@ -4,17 +4,39 @@ from starlette.requests import Request
 from starlette.responses import Response
 from starlette.status import HTTP_401_UNAUTHORIZED
 from utils.security import checkJWTToken
+from fastapi.middleware.cors import CORSMiddleware
 
 server = FastAPI()
 
 server.mount("/v1", server_v1)
 
+origins = [
+    "http://localhost",
+    "http://localhost:8080/",
+    "http://localhost:8080"
+]
+
+server.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["authorization", "Authorization" "Accept", "Accept-Language", "Content-Language", "Content-Type"],
+    #expose_headers=["*"]
+)
+
 
 @server.middleware("http")
 async def middleware(request: Request, callNext):
     if not any(word in str(request.url) for word in ["/login", "/newUser", "/docs", "/openapi.json"]):
+        if request.method == "OPTIONS":
+            return Response("ok", status_code=200, headers={"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Credentials": "true", "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT,DELETE,UPDATE", "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, authorization, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"})
         try:
-            jwtToken = request.headers["Authorization"].split(" ")[1]
+            print("hiii")
+            print(request.method)
+            print(request.headers["Authorization"])
+            jwtToken = request.headers["Authorization"].split(" ")[1] # Authorization
+            print(jwtToken)
 
             if not await checkJWTToken(jwtToken):
                 return Response("Unauthorized", status_code=HTTP_401_UNAUTHORIZED)
